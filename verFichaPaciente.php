c27a2b7f79dcbfbc3d1b68812d1f868fd518984e
victor duarte <duarte1111_@hotmail.com>
2013-06-26 03:44:40 -0400

    ver ficha paciente
    permite ver la ficha de un paciente

---
+++ verFichaPaciente.php
@@ -0,0 +1,58 @@
+<?php
+	include 'Clases/db.php';
+	include 'Clases/tipos_Usuarios.php';
+	include 'Clases/usuario.php';
+	if (isset($_GET['rut']) == false)
+		$rut_id = 0;	
+	else
+		$rut_id = $_GET['rut'];
+	$usuario = new usuario($rut_id);
+	
+	if($usuario->existeUsuarioDB() == true){
+		$usuario->cargarUsuarioDB();
+		if  ($usuario->usuarioPerteneceAPerfil(3)){		
+			?>
+			<table width="200" border="1">
+			  <tr>
+				<td>Nombres</td>
+				<td><?php echo $usuario->getNombre();  ?></td>
+			  </tr>
+				<tr>
+				<td>Apellidos</td>
+				<td><?php echo $usuario->getApellidos(); ?></td>
+			  </tr>
+			  <tr>
+				<td>Direccion</td>
+				<td><?php echo $usuario->getDireccion(); ?></td>
+			  </tr>
+			  <tr>
+				<td>Telefono</td>
+				<td><?php echo $usuario->getTelefono(); ?></td>
+			  </tr>
+			  <tr>
+				<td>Rut</td>
+				<td><?php echo $usuario->getRutUsuario();?></td>
+			  </tr>
+			  <tr>
+				<td>E-mail</td>
+				<td><?php echo $usuario->getEmail();?></td>
+			  </tr>
+			  <tr>
+				<td>Fecha Nacimiento</td>
+				<td><?php echo $usuario->getFechaNacimiento(); ?></td>
+			  </tr>
+			  <tr>
+				<td>Contrasena</td>
+				<td>*********</td>
+			  </tr>
+			  </table>
+			
+			<?php
+		}else{
+			echo "el usuario no es un paciente del sitema";
+		}
+	}
+	else{
+		echo "el usuario con el Rut {$usuario->getRutUsuario()}, no existe";
+	}
+?>
