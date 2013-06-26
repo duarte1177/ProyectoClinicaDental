3be2e987f606148fa4d6fe692f406e8dc0ae6283
victor duarte <duarte1111_@hotmail.com>
2013-06-26 03:42:41 -0400

    index
    index de la pagina

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<HTML xmlns="http://www.w3.org/1999/xhtml">
+<HEAD>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<TITLE><H1>ClINICA DENTAL "SONRRISA HERMOSA" </H1></TITLE>
+</HEAD>
+	<BODY>
+    	<br />
+			<h3>Modificar Paciente</h3>
+				<form action="modificarFichaPaciente.php" method="get">
+					<input type="text" name="rutUsuarioModificar" /><br />
+					<input type="submit"  />
+				</form>
+		<br />
+			<h3><a href="crearFichaPaciente.php"> Crear Nuevo Paciente</a>
+			</h3>
+    	<br />
+			<h3> Ver Ficah Pacienrte</h3>
+				<form action="verFichaPaciente.php" method="get">
+					<input type="text" name="rut" />
+		<br />
+				<input type="submit"  />
+				</form>
+	</BODY>	
+</HTML>
