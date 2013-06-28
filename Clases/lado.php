3024470c184eec488b6dcad8a4d58ec0c297f5e2
victor duarte <duarte1111_@hotmail.com>
2013-06-28 01:09:41 -0400

    lado
    clase

--- Clases/lado.php
+++ Clases/lado.php
@@ -1,8 +1,25 @@
 <?php
 	class lado{
-		private id_Lado;
-		private id_Pieza;
-		private descripcion;
+		private $id_Lado;
+		private $id_Pieza;
+		private $descripcion;
+		
+		public function __Construct($id_Lado, $id_Pieza = NULL, $descripcion = NULL){
+			$this->idLado = $id_Lado;
+			$this->idPieza = $id_Pieza;
+			$this->descripcion = $descripcion;
+		}
+		
+		public function cargarLadoDB(){
+		
+		$db = DB::getInstance();
+		$sql= "SELECT * FROM  `lados` WHERE  `ID_LADO` = $this->idLado LIMIT 1";
+		$db->setQuery($sql);
+		$lado = $db->loadObjectList();
+		$this->idLado = $lado[0]->ID_LADO;
+		$this->idPieza = $lado[0]->ID_PIEZA;
+		$this->descripcion = $lado[0]->DESCRIPCION;
+	}
 		
 		public function setIdLado($id_LadoNuevo){
 			$this->idLado = $id_LadoNuevo;
