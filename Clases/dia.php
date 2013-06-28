23a92a400e4d560e5994cbe58611ff4b4503d184
victor duarte <duarte1111_@hotmail.com>
2013-06-28 13:23:19 -0400

    dia
    modificaciones a la clases dia

--- Clases/dia.php
+++ Clases/dia.php
@@ -1,10 +1,12 @@
 <?PHP
 	class dia{
 		private $id_Dia;
+		private $mes;
 		private $id_Mes;
 		
 		public function __Construct($id_Dia, $id_Mes = NULL){
 			$this->idDia = $id_Dia;
+			$this->mes = $mes;
 			$this->idMes = $id_Mes;
 		}
 		
@@ -15,6 +17,7 @@
 		$db->setQuery($sql);
 		$dia = $db->loadObjectList();
 		$this-> idDia = $dia[0]->ID_DIA;
+		$this-> mes = $dia[0]->MES;
 		$this-> idMes = $dia[0]->ID_MES;
 		}
 	
@@ -22,12 +25,18 @@
 		public function setIdDia($id_DiaNuevo){
 			$this->idDia = $id_DiaNuevo;
 		}
+		public function setMes($mesNuevo){
+			$this->mes = $mesNuevo;
+		}
 		public function setIdMes($id_MesNuevo){
 			$this->idMes = $id_MesNuevo;
 		}
 		
 		public function getIdDia(){
 			return $this->idDia;
+		}
+		public function getMes(){
+			return $this->mes;
 		}
 		public function getIdMes(){
 			return $this->idMes;
